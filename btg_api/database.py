edc1e5c4008cdbffa459d19f54f5da2bac6488cd
fasaor <fasaor0423@gmail.com>
2025-09-12 20:03:57 -0500

    Update database.py

--- btg_api/database.py
+++ btg_api/database.py
@@ -1,8 +1,8 @@
-import os
 from pymongo import MongoClient
+import certifi
+import os
 
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
+client = MongoClient(os.getenv("MONGO_URI"), tlsCAFile=certifi.where())
 db = client["btg_db"]
 
 clientes_collection = db["clientes"]

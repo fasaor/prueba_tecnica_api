f268078e0812f1c16c603d8f5f80f50356972680
fasaor <fasaor0423@gmail.com>
2025-09-12 20:24:27 -0500

    Update fondos.py

--- btg_api/routers/fondos.py
+++ btg_api/routers/fondos.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
-from models import Fondo
 from database import fondos_collection
 
 router = APIRouter()
 
 @router.get("/")
 def listar_fondos():
-    fondos = list(fondos_collection.find({}, {"_id": 0}))
-    return fondos
+    return list(fondos_collection.find({}, {"_id": 0}))
